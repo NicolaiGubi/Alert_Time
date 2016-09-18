b08f4eb5bbb5e75b35edb04ac8a226c066638a5c
NicolaiGubi <nicolaigubi@gmail.com>
2016-09-18 22:13:55 +0200

    Added files
    Added
    Alert_Playback_Loop
    Alert_Playback_Stop

---
+++ Alert_Playback_Loop.lua
@@ -0,0 +1,21 @@
+get_var = gma.show.getvar
+cmd = gma.cmd
+function feedback_test ()
+active_cue = get_var('SELECTEDEXECCUE')
+current_cue = active_cue 
+looper = 1
+	while looper == 1 do
+		if active_cue ~= current_cue then
+			cmd ('top timer 4')
+			cmd ('Assign Timer 4 /countdowntime='..a[active_cue])
+			cmd ('go timer 4')
+			current_cue = active_cue
+		else
+			gma.feedback('We wait.')
+			gma.sleep(0.25)
+			active_cue = get_var('SELECTEDEXECCUE')
+		end
+	end
+end
+
+return feedback_test
---
+++ Alert_Playback_Stop.lua
@@ -0,0 +1,7 @@
+function EndLoop ()
+	looper = 2
+	gma.cmd('Off Plugin 25')
+	gma.cmd('Off Timer Thru')
+end
+
+return EndLoop
